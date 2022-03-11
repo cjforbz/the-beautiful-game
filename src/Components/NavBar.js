bc96d7a2af62c901578d8353f07265e9e18b3234
Colin Forbes <cjforbes32@gmail.com>
2022-03-10 19:06:10 -0500

    rudimentary nav bar built

---
+++ src/Components/NavBar.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function NavBar() {
+  return (
+    <div className="nav-bar">
+      <nav id="nav-bar">
+        <h3>The Beautiful Game</h3>
+      </nav>
+    </div>
+  );
+}
+export default NavBar;
